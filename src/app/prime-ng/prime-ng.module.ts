dc3de207abee6336e3b42cbfb095f0846d70408a
CORP\pagarcia <pablo.garcia-simavillal@capgemini.com>
2024-10-22 13:42:46 +0200

    Menu bar

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,10 +1,12 @@
 import { MenuModule } from 'primeng/menu';
+import { MenubarModule } from 'primeng/menubar';
 import { NgModule } from '@angular/core';
 
 
 @NgModule({
   declarations: [],
   exports: [
+    MenubarModule,
     MenuModule
   ]
 })
--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -13,9 +13,34 @@ export class MenuComponent implements OnInit{
   ngOnInit(): void {
     console.log("MenuComponent cargado");
     this.menuItems = [
-      {label: 'New', icon: 'pi pi-fw pi-plus'},
-      {label: 'Open', icon: 'pi pi-fw pi-download'},
-      {label: 'Undo', icon: 'pi pi-fw pi-refresh'},
+      {
+        label: 'Pipes de Angular',
+        icon: 'pi pi-desktop',
+        items: [
+          {
+            label: 'Textos y Fechas',
+            icon: 'pi pi-align-left',
+          },
+          {
+            label: 'Números',
+            icon: 'pi pi-dollar',
+          },
+          {
+            label: 'No Comunes',
+            icon: 'pi pi-globe',
+          },
+        ]
+      },
+      {
+        label: 'Pipes Personalizados',
+        icon: 'pi pi-cog',
+        items: [
+          {
+            label: 'Otro Elemento'
+          }
+        ]
+      }
+
     ];
   }
 }
