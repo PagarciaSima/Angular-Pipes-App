af8e73cb2cfbff0d76c11cc1f2b40aa7f74addb6
CORP\pagarcia <pablo.garcia-simavillal@capgemini.com>
2024-10-24 14:04:24 +0200

    i18nplural y select pipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  // i18nSelect
+  public name: string = 'Pablo';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    'male': 'invitarlo',
+    'female': 'invitarla'
+  }
+
+  changeClient(): void {
+    if (this.name === 'Pablo') {
+      this.name = 'Ana';
+      this.gender = 'female';
+    } else {
+      this.name = 'Pablo';
+      this.gender = 'male';
+    }
+  }
+
+  // i18nPlural
+  public clients: string [] = ['Maria', 'Pedro', 'Fernando', 'Hernando', 'Eduardo', 'Melissa', 'Natalia'];
+  public clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    'other': 'tenemos # clientes esperando'
+  }
+
+  deleteClient():void {
+    this.clients.shift();
+  }
 }
